84ca6bd641d96c5b175ebef957ec628c61b8c1fb
Nazar Yurakh <nazar.yurakh@gmail.com>
2026-07-24 16:59:43 +0200

    fix: correct date parsing and error handling in get_days_from_today

--- task_1.py
+++ task_1.py
@@ -1,11 +1,21 @@
 import datetime
 
-def get_days_from_today(date):
-    today = datetime.date.today()
-    date_obj = datetime.datetime.strptime(date, '%Y-%m-%d').date()
-    delta = today - date_obj
-    return delta.days
+def get_days_from_today(date: str) -> int | None:
+    """Розраховує кількість днів між поточною датою та заданою датою."""
+    try:
+        target_date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+        today = datetime.date.today()
 
-date = '2026-07-17'
+        return (today - target_date).days
+    except ValueError:
+        print(f"Неправильний формат дати: {date}. Використовуйте формат 'YYYY-MM-DD'.")
+        return None
+        
+    
+    
+    
 
-print(get_days_from_today(date))
+
+print(get_days_from_today('2026-05-24'))
+print(get_days_from_today('2026-10-12'))
+print(get_days_from_today("2026-11-45"))
