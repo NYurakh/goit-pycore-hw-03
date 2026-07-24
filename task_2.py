db5d1d7ad13ae9ca2d9aaf015d9c7835cc345f76
Nazar Yurakh <nazar.yurakh@gmail.com>
2026-07-24 17:16:05 +0200

    feat: add get_numbers_ticket function (#2)

---
+++ task_2.py
@@ -0,0 +1,24 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    """Генерує список унікальних випадкових чисел в заданому діапазоні."""
+    if (
+        min < 1
+        or max > 1000
+        or min > max
+        or quantity < 1
+        or quantity > (max - min + 1)
+    ):
+        return []
+    numbers = random.sample(range(min, max + 1), quantity)
+    return sorted(numbers)
+
+# Перевірка функції
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
+
+# Некоректні дані для перевірки обмежень
+print("Невалідний min:", get_numbers_ticket(0, 49, 6))
+print("Невалідний max:", get_numbers_ticket(1, 1001, 6))
+print("Невалідна quantity:", get_numbers_ticket(1, 49, 0))
+print("Завелика quantity:", get_numbers_ticket(1, 49, 50))
